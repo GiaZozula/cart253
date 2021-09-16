167728446639da97af2fba5a9fd74941d621d88e
olstinker <giazozula@gmail.com>
2021-09-16 10:43:53 -0400

    drew the alien

--- activities/02-drawn-an-alien/js/script.js
+++ activities/02-drawn-an-alien/js/script.js
@@ -1,9 +1,8 @@
 /**
-Title of Project
-Author Name
+activity 2 - drawn an alien
+Gia <3
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
 */
 
 "use strict";
@@ -21,6 +20,35 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(640, 480);
+
+//orange background
+  background(255, 110, 25);
+  noStroke();
+
+//draw the body
+  fill(127);
+  ellipse(320,480,300,200);
+
+//draw a purple head
+  fill(128, 74, 148);
+  ellipse(320,240,250,400);
+
+//draw the eyes
+fill(0);
+ellipse(250,240,80,250);
+ellipse(390,240,80,250);
+
+//draw the nostrils
+  fill(0);
+  ellipse(300, 350, 10, 10);
+  ellipse(340, 350, 10, 10);
+
+//draw the mouth
+  stroke(234, 178, 237);
+  strokeWeight(12);
+  rectMode(CENTER);
+  rect(320,400,100,20);
 
 }
 
