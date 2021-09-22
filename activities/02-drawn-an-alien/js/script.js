a2a44b9eb2622769c431cec556c8f48ea2d2987b
olstinker <giazozula@gmail.com>
2021-09-22 18:25:34 -0400

    E3: Added background, nostroke

--- activities/02-drawn-an-alien/js/script.js
+++ activities/02-drawn-an-alien/js/script.js
@@ -7,14 +7,12 @@ Draws an alien on canvas
 
 "use strict";
 
-
 /**
 Description of preload
 */
 function preload() {
-
+  loadModel("assets/funblob.obj", true);
 }
-
 
 /**
 Draws an alien
@@ -22,58 +20,52 @@ Draws an alien
 function setup() {
   createCanvas(640, 480);
 
-//orange background
+  //orange background
   background(149, 227, 230);
   noStroke();
 
-//draw the body
+  //draw the body
   fill(98, 29, 99);
-  ellipse(320,480,300,200);
+  ellipse(320, 480, 300, 200);
 
-//draw a purple head
-fill(128, 74, 148);
-ellipse(320,240,250,400);
+  //draw a purple head
+  fill(128, 74, 148);
+  ellipse(320, 240, 250, 400);
+  ellipse(320, 240, 400, 200);
 
-ellipse(320,240,400,200);
+  //draw the eyes
+  fill(68, 17, 128);
+  ellipse(250, 240, 80, 80);
+  ellipse(390, 240, 80, 80);
 
-//draw the eyes
-fill(68, 17, 128);
-ellipse(250,240,80,80);
-ellipse(390,240,80,80);
-
-//draw the eyes
-fill(236, 194, 237);
-ellipse(250,240,40,40);
-ellipse(390,240,40,40);
+  //draw the eyes
+  fill(236, 194, 237);
+  ellipse(250, 240, 40, 40);
+  ellipse(390, 240, 40, 40);
 
   //draw the nose
   fill(234, 178, 237);
   noStroke();
   triangle(280, 350, 350, 320, 300, 190);
 
-//draw the nostrils
+  //draw the nostrils
   fill(0);
   ellipse(300, 340, 10, 10);
   ellipse(335, 325, 10, 10);
 
-//draw the mouth
+  //draw the mouth
   stroke(234, 178, 237);
   strokeWeight(12);
   rectMode(CENTER);
-  rect(320,400,100,20,20,20,20);
+  rect(320, 400, 100, 20, 20, 20, 20);
 
   //draw some eyebrows
   noFill();
   arc(280, 100, 100, 55, PI, PI + QUARTER_PI);
   arc(400, 80, 100, 55, HALF_PI, PI);
-
-
 }
-
 
 /**
 does nothing
 */
-function draw() {
-
-}
+function draw() {}
--- exercises/i-like-to-movie-it/js/script.js
+++ exercises/i-like-to-movie-it/js/script.js
@@ -1,33 +1,27 @@
 /**
-Title of Project
-Author Name
+~~I Like to move it, move it~~
+Gia :)
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Gia's program for exercise #1 "I like to move it"
 */
 
 "use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(640, 480);
 
+  //baby blue background
+  background(149, 227, 230);
+  noStroke();
 }
-
 
 /**
 Description of draw()
-*/
-function draw() {
 
-}
+ideas:
+random number + variable (size, position, alpha) in relation to mouse, for a bunch of little circles and lines that follow the mouse
+*/
+function draw() {}
