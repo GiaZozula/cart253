9db0f1765d42e453622d4b948a793ffb62c0c5a5
olstinker <giazozula@gmail.com>
2021-09-23 16:11:27 -0400

    starting from scratch

--- exercises/i-like-to-movie-it/js/script.js
+++ exercises/i-like-to-movie-it/js/script.js
@@ -1,48 +1,24 @@
 /**
-~~I Like to move it, move it~~
-Gia :)
+Title of Project
+Author Name
 
-Gia's program for exercise #1 "I like to move it"
+This is a template. You must fill in the title,
+author, and this description to match your project!
 */
 
 "use strict";
 
 /**
+Description of preload
+*/
+function preload() {}
+
+/**
 Description of setup
 */
-let rectangle = {
-  x: 0,
-  y: 0,
-  size: 50,
-  vx: 0,
-  vy: 0,
-  shade: 255,
-};
-
-function setup() {
-  createCanvas(500, 500);
-
-  rectangle.vx = 12;
-  rectangle.vy = 12;
-
-  //baby blue background
-  background(0);
-}
+function setup() {}
 
 /**
 Description of draw()
-
-ideas:
-random number + variable (size, position, alpha) in relation to mouse, for a bunch of little circles and lines that follow the mouse
 */
-function draw() {
-  rectangle.x += rectangle.vx;
-  rectangle.y += rectangle.vy;
-  rectangle.size = rectangle.size + -2;
-  rectangle.shade = rectangle.shade + random(-1, -30);
-
-  rectMode(CENTER);
-  noFill();
-  stroke(rectangle.shade);
-  rect(rectangle.x, rectangle.y, rectangle.size, rectangle.size);
-}
+function draw() {}
