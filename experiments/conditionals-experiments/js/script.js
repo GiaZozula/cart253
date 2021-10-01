4cb298773e4b06868f7025d3f8c94a5a4bf2c8fc
olstinker <giazozula@gmail.com>
2021-09-30 22:04:27 -0400

    Finished  4.4

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,4 +1,8 @@
-let displayCircle = false;
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segmentSize: 50,
+};
 
 function setup() {
   createCanvas(500, 500);
@@ -6,13 +10,26 @@ function setup() {
 
 function draw() {
   background(0);
+  noStroke();
+  fill(100, 200, 100);
 
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
+  // let x = caterpillar.x;
+  // let numSegments = 5;
+  // let segmentsDrawn = 0;
+  //
+  // while (segmentsDrawn < numSegments) {
+  //   ellipse(x, caterpillar.y, caterpillar.segmentSize);
+  //   x = x + 40;
+  //   segmentsDrawn++;
+  // }
 
-  if (displayCircle) {
-    ellipse(250, 250, 100, 100);
+  let x = caterpillar.x;
+  let numSegments = 10;
+
+  for (let i = 0; i < numSegments; i++) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 40;
   }
 }
+
 // >, <, >=, <=, ===, !==
