1e9a94c9adac293f30ff123b5bdbac70b1d1768b
olstinker <giazozula@gmail.com>
2021-09-30 21:15:29 -0400

    finished following along with 4.1

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,33 +1,37 @@
-/**
-Title of Project
-Author Name
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 5,
+};
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(500, 500);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(backgroundShade);
 
+  circle.x += circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0) {
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height / 2) {
+    fill(255, 0, 0);
+  }
+
+  if (mouseY > height / 2) {
+    fill(0, 0, 255);
+  }
+
+  ellipse(circle.x, circle.y, circle.size);
 }
+
+// >, <, >=, <=, ===, !==
