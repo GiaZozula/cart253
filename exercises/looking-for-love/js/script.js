6b00100f76369062b9c7f2a577b6dbeda4c09d93
olstinker <giazozula@gmail.com>
2021-10-07 18:48:14 -0400

    Beginning my modifications for the exercise
    Added a third circle attached to the mouse, changed the movement of the other two circles to be based off of noise

--- exercises/looking-for-love/js/script.js
+++ exercises/looking-for-love/js/script.js
@@ -10,20 +10,30 @@ author, and this description to match your project!
 
 let circle1 = {
   x: undefined,
-  y: 250,
-  size: 100,
+  y: 100,
+  size: 50,
   vx: 0,
   vy: 0,
-  speed: 3,
+  speed: 0.2,
+  tx: 130,
+  ty: 11,
 };
 
 let circle2 = {
   x: undefined,
-  y: 250,
-  size: 100,
+  y: 400,
+  size: 50,
   vx: 0,
   vy: 0,
-  speed: 3,
+  speed: 0.2,
+  tx: 400,
+  ty: 10,
+};
+
+let circle3 = {
+  x: undefined,
+  y: undefined,
+  size: 100,
 };
 
 let state = `title`; //Can be title, simulation, love, and sadness
@@ -43,15 +53,15 @@ function setup() {
 
 function setupCircles() {
   // position circles seperated from one another
-  circle1.x = width / 3;
-  circle2.x = (2 * width) / 3;
+  circle1.x = width / 5;
+  circle2.x = (2 * width) / 5;
 
-  // start circles moving in a random direction
-  circle1.vx = random(-circle1.speed, circle1.speed);
-  circle2.vx = random(-circle2.speed, circle2.speed);
-
-  circle1.vy = random(-circle1.speed, circle1.speed);
-  circle2.vy = random(-circle2.speed, circle2.speed);
+  // // start circles moving in a random direction
+  // circle1.vx = random(-circle1.speed, circle1.speed);
+  // circle2.vx = random(-circle2.speed, circle2.speed);
+  //
+  // circle1.vy = random(-circle1.speed, circle1.speed);
+  // circle2.vy = random(-circle2.speed, circle2.speed);
 }
 
 /**
@@ -107,11 +117,26 @@ function sadness() {
 
 function move() {
   //move the circles
-  circle1.x += circle1.vx;
-  circle1.y += circle1.vy;
+  // circle1.x += circle1.vx;
+  // circle1.y += circle1.vy;
+  //
+  // circle2.x += circle2.vx;
+  // circle2.y += circle2.vy;
 
-  circle2.x += circle2.vx;
-  circle2.y += circle2.vy;
+  circle1.x = map(noise(circle1.tx), 0, 1, 0, width);
+  circle1.y = map(noise(circle1.ty), 0, 1, 0, height);
+
+  circle2.x = map(noise(circle2.tx), 0, 1, 0, width);
+  circle2.y = map(noise(circle2.ty), 0, 1, 0, height);
+
+  circle1.tx += 0.0003;
+  circle1.ty += 0.003;
+  circle2.tx += 0.002;
+  circle2.ty += 0.0002;
+
+  //mouse control of circle3
+  circle3.x = mouseX;
+  circle3.y = mouseY;
 }
 
 function checkOffscreen() {
@@ -141,6 +166,12 @@ function display() {
   //display the circles
   ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
+
+  //display circle3 and give it a colour
+  push();
+  fill(255, 30, 30);
+  ellipse(circle3.x, circle3.y, circle3.size);
+  pop();
 }
 
 function mousePressed() {
