4b1c9c2c678b75a3668e24d97c00d6e7cf13d0de
olstinker <giazozula@gmail.com>
2021-10-07 17:33:40 -0400

    Still following along with Pippin
    Did some copy and pasting to clean up the code and add functions

--- exercises/looking-for-love/js/script.js
+++ exercises/looking-for-love/js/script.js
@@ -55,33 +55,47 @@ Description of draw()
 function draw() {
   background(0);
 
+  move();
+  checkOffscreen();
+  checkOverlap();
+  display();
+}
+
+function move() {
   //move the circles
   circle1.x += circle1.vx;
   circle1.y += circle1.vy;
 
   circle2.x += circle2.vx;
   circle2.y += circle2.vy;
+}
 
+function checkOffscreen() {
   //check if the circles have gone offscreen
   if (
     circle1.x < 0 ||
     circle1.x > width ||
-    circle.y < 0 ||
+    circle1.y < 0 ||
     circle1.y > height ||
     circle2.x < 0 ||
     circle2.x > width ||
     circle2.y < 0 ||
     circle2.y > height
-    //SAD ENDING
-  )
-
-  //checj if circles overlap
-  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
-  if (d < circle1.size/2 + circle2.size/2){
-    //LOVE ENDING
+    //SAD ENDING!
+  ) {
   }
+}
 
-    //display the circles
+function checkOverlap() {
+  //checj if circles overlap
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size / 2 + circle2.size / 2) {
+    //LOVE ENDING!
+  }
+}
+
+function display() {
+  //display the circles
   ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
 }
