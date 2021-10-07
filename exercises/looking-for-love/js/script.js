875ed3de52417fdef67833605c8785bf0b1447a6
olstinker <giazozula@gmail.com>
2021-10-07 17:14:33 -0400

    Following along with Pippin
    Added a bunch of things, currently there are two circles that randomly move. About half way through Pippin's video.

--- exercises/looking-for-love/js/script.js
+++ exercises/looking-for-love/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+~~looking for luvv!~~ <3 <3
+Gia
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,80 @@ author, and this description to match your project!
 
 "use strict";
 
+let circle1 = {
+  x: undefined,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
+
+let circle2 = {
+  x: undefined,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(500, 500);
 
+  // position circles seperated from one another
+  circle1.x = width / 3;
+  circle2.x = (2 * width) / 3;
+
+  // start circles moving in a random direction
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle2.vx = random(-circle2.speed, circle2.speed);
+
+  circle1.vy = random(-circle1.speed, circle1.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  //move the circles
+  circle1.x += circle1.vx;
+  circle1.y += circle1.vy;
+
+  circle2.x += circle2.vx;
+  circle2.y += circle2.vy;
+
+  //check if the circles have gone offscreen
+  if (
+    circle1.x < 0 ||
+    circle1.x > width ||
+    circle.y < 0 ||
+    circle1.y > height ||
+    circle2.x < 0 ||
+    circle2.x > width ||
+    circle2.y < 0 ||
+    circle2.y > height
+    //SAD ENDING
+  )
+
+  //checj if circles overlap
+  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+  if (d < circle1.size/2 + circle2.size/2){
+    //LOVE ENDING
+  }
+
+    //display the circles
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
 }
