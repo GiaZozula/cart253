487c1378a7ee9bec46ddf8c9917841ffe679127d
olstinker <giazozula@gmail.com>
2021-10-14 16:18:47 -0400

    Setting up circle playfield, canvas size, and radius

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -1,33 +1,51 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+WIZARD HELL
+By Gia <3
+description to match your project!
 */
 
 "use strict";
 
+let radius = 350;
+let angle = 0;
+let speed = 0.03;
+
+// the center of our rotation:
+let centerX = 700;
+let centerY = 400;
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(1400, 800);
+  rectMode(CENTER);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  //drawing the "playfield"
+  circle(centerX, centerY, radius * 2);
+
+  //computing the angle
+  // text(angle, 50, 50);
+
+  // translate to point to rotate around
+  translate(centerX, centerY);
+  rotate(angle);
+  // draw shape as though (centerX, centerY) is the new
+  // origin / (0, 0) point
+  rect(radius, 0, 50, 50); //add this to "USER"
+  line(0, 0, radius, 0); //add this to "shotLine"
+
+  angle = angle + speed;
 }
